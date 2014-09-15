e58aac904b6a9ef520734ff2cd164de6e3d2be64
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2014-09-15 18:40:49 +0100

    File
    Changing the file code.

--- TCP/tcpclient.c
+++ TCP/tcpclient.c
@@ -8,32 +8,56 @@
 
 #define PORT 58000
 
-int main()
+int main(int argc, char *argv[])
 {
-  int fd, newfd;
-  struct hostent *hostptr;
-  struct sockaddr_in serveraddr, clientaddr;
-  int clientlen;
-
-  fd = socket(AF_INET, SOCK_STREAM, 0);
-  hostptr = gethostbyname("localhost");
-
-  memset((void *)&serveraddr, '\0', sizeof(serveraddr));
-  serveraddr.sin_family = AF_INET;
-  serveraddr.sin_addr.s_addr = ((struct in_addr *)(hostptr->h_addr_list[0]))->s_addr;
-  serveraddr.sin_port = htons((unsigned short)PORT);
-
-  connect(fd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
-
-  write(fd, "hello", 6);
-  printf("[Client] Sent: Yes\n");
-
-  char buf[20];
-  read(fd, buf, 8); // expect "Close"
-
-  printf("[Client] Received: %s\n", buf);
-
-  close(fd);
-
-  return 0;
+	int clientlen;
+	int fd, newfd;
+	
+	char *hname = "localhost";
+	
+	struct hostent *hostptr;
+	struct sockaddr_in serveraddr, clientaddr;
+	
+	if (argc > 1)
+	{
+		hname = argv[1];
+	}
+	
+	fd = socket(AF_INET, SOCK_STREAM, 0);
+	hostptr = gethostbyname(hname);
+	
+	memset((void *)&serveraddr, '\0', sizeof(serveraddr));
+	
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = ((struct in_addr *)(hostptr->h_addr_list[0]))->s_addr;
+	serveraddr.sin_port = htons((unsigned short)PORT);
+	
+	connect(fd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+	
+	for (;;)
+	{
+		char buf[100];
+		if (scanf("%s", buf) == EOF)
+		{
+			break;
+		}
+		
+		buf[99] = '\0';
+		
+		write(fd, buf, sizeof(buf) - 1);
+		// printf("[Client] Sent: hello\n");
+		
+		printf("Server reply:\n");
+		
+		while (read(fd, buf, sizeof(buf) - 1) > 0)
+		{
+			printf("%s", buf);
+		}
+		
+		printf("\n");
+	}
+	
+	close(fd);
+	
+	return 0;
 }
